b275367bfe8adf593ff3a6ab40f00bed88d17984
Jongwoop <58589069+Jongwoop@users.noreply.github.com>
2021-05-02 11:09:56 +0300

    JS: arrow up

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 "use strict";
 
+// Make navbar button to scroll into each section
+const navbarMenu = document.querySelector(".navbar__menu");
+
+navbarMenu.addEventListener("click", (event) => {
+  const link = event.target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
@@ -21,17 +32,6 @@ document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-// Scroll to section
-const navbarMenu = document.querySelector(".navbar__menu");
-
-navbarMenu.addEventListener("click", (event) => {
-  const link = event.target.dataset.link;
-  if (link == null) {
-    return;
-  }
-  scrollIntoView(link);
-});
-
 // Handle click on "contact me" button on home
 const contactBtn = document.querySelector(".home__contact");
 contactBtn.addEventListener("click", () => {
@@ -43,4 +43,16 @@ function scrollIntoView(selector) {
   scrollTo.scrollIntoView({ behavior: "smooth", block: "center" });
 }
 
-//
+// Show "Arrow up" button when scrolling down
+const arrowBtn = document.querySelector(".arrow-up");
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowBtn.classList.add("visible");
+  } else {
+    arrowBtn.classList.remove("visible");
+  }
+});
+arrowBtn.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
